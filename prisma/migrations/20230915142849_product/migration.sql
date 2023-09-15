f89831394e8b3f08c43b35d4b5fd543f5419ea20
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2023-09-15 11:29:13 -0300

    add migration product and add db

---
+++ prisma/migrations/20230915142849_product/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "Server" ADD COLUMN     "productId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+
+-- AddForeignKey
+ALTER TABLE "Server" ADD CONSTRAINT "Server_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE SET NULL ON UPDATE CASCADE;
