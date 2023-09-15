2cad1813c5ae9d3de4e215371e981cdf692bd088
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2023-09-15 12:19:30 -0300

    fix database and migration product

--- prisma/migrations/20230915151340_product/migration.sql
+++ prisma/migrations/20230915151340_product/migration.sql
@@ -4,11 +4,15 @@ ALTER TABLE "Server" ADD COLUMN     "productId" INTEGER;
 -- CreateTable
 CREATE TABLE "Product" (
     "id" SERIAL NOT NULL,
+    "productId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_productId_key" ON "Product"("productId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
