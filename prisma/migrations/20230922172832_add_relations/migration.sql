e5db46897a889ed9d2fe3c0ea5b3df569e34a610
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2023-09-22 14:28:47 -0300

    fix database

--- prisma/migrations/20230922172832_add_relations/migration.sql
+++ prisma/migrations/20230922172832_add_relations/migration.sql
@@ -4,6 +4,7 @@
   - You are about to drop the column `productId` on the `Server` table. All the data in the column will be lost.
   - You are about to drop the column `serverId` on the `ServerOwner` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[serverOwnerId]` on the table `Server` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `serverId` to the `Product` table without a default value. This is not possible if the table is not empty.
   - Added the required column `serverOwnerId` to the `Server` table without a default value. This is not possible if the table is not empty.
 
 */
@@ -14,7 +15,7 @@ ALTER TABLE "Server" DROP CONSTRAINT "Server_productId_fkey";
 ALTER TABLE "ServerOwner" DROP CONSTRAINT "ServerOwner_serverId_fkey";
 
 -- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "serverId" INTEGER;
+ALTER TABLE "Product" ADD COLUMN     "serverId" INTEGER NOT NULL;
 
 -- AlterTable
 ALTER TABLE "Server" DROP COLUMN "productId",
@@ -30,4 +31,4 @@ CREATE UNIQUE INDEX "Server_serverOwnerId_key" ON "Server"("serverOwnerId");
 ALTER TABLE "Server" ADD CONSTRAINT "Server_serverOwnerId_fkey" FOREIGN KEY ("serverOwnerId") REFERENCES "ServerOwner"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Product" ADD CONSTRAINT "Product_serverId_fkey" FOREIGN KEY ("serverId") REFERENCES "Server"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Product" ADD CONSTRAINT "Product_serverId_fkey" FOREIGN KEY ("serverId") REFERENCES "Server"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
