432051e0a97e4d0785324b0869e17ab2cc80583a
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2023-09-29 14:57:22 -0300

    fix import

--- src/domain/payment/factory/payment.facade.factory.ts
+++ src/domain/payment/factory/payment.facade.factory.ts
@@ -1,4 +1,4 @@
-import PaymentRepository from '../../../infrastructure/payment/repository/payment.repository';
+import PaymentRepository from '../../../infrastructure/payment/repositories/payment.repository';
 import PaymentProcessUseCase from '../../../usecase/payment/create/create.payment.usecase';
 import PaymentFacade from '../facade/payment.facade';
 import PaymentFacadeInterface from '../facade/payment.facade.interface';
