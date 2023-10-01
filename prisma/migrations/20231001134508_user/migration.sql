81555a48e9c3bf91f25d7e6965d6bc2e7355882f
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2023-10-01 10:45:26 -0300

    add user in database

---
+++ prisma/migrations/20231001134508_user/migration.sql
@@ -0,0 +1,27 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "userId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "Server" ADD COLUMN     "userId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userId_key" ON "User"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Server" ADD CONSTRAINT "Server_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
