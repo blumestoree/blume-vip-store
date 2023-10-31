74f7fb75334f2e32adc4c4d3872803447549d010
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2023-10-31 13:21:16 -0300

    fix product test

--- src/domain/product/factory/product.factory.test.ts
+++ src/domain/product/factory/product.factory.test.ts
@@ -3,17 +3,19 @@ import ProductFactory from './product.factory';
 
 describe('Product factory unit test', () => {
   it('should create a product', () => {
-    const server = ProductFactory.create('name', 100, 'serverId');
+    const server = ProductFactory.create('name', 'image', 100, 'serverId');
     expect(server.id).not.toBeNull();
     expect(server.name).toBe('name');
+    expect(server.image).toBe('image');
     expect(server.price).toBe(100);
     expect(server.serverId).toBe('serverId');
   });
 
   it('should create a server with id', () => {
-    const server = ProductFactory.create('name', 100, 'serverId', 'id');
+    const server = ProductFactory.create('name', 'image', 100, 'serverId', 'id');
     expect(server.id).toEqual('id');
     expect(server.name).toBe('name');
+    expect(server.image).toBe('image');
     expect(server.price).toBe(100);
     expect(server.serverId).toBe('serverId');
   });
