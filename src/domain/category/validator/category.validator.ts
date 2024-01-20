097891b2c677626e38868baba5411b13f558770e
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-01-20 17:19:06 -0300

    fix name schema validator

--- src/domain/category/validator/category.validator.ts
+++ src/domain/category/validator/category.validator.ts
@@ -5,7 +5,7 @@ import * as z from 'zod';
 
 export default class CategoryValidator implements ValidatorInterface<Category> {
   validate(entity: Category): ValidationError[] | void {
-    const productSchema = z.object({
+    const categorySchema = z.object({
       _id: z.string(),
       _name: z.string().min(2, 'Nome inválido'),
       _functionInGame: z.string(),
@@ -13,7 +13,7 @@ export default class CategoryValidator implements ValidatorInterface<Category> {
       _products: z.any()
     });
     try {
-      productSchema.parse(entity);
+      categorySchema.parse(entity);
     } catch (error) {
       const zodError = error as z.ZodError;
       const errorMessages = zodError.errors.map((issue) => issue.message);
--- src/domain/payment/validator/payment.validator.ts
+++ src/domain/payment/validator/payment.validator.ts
@@ -5,13 +5,13 @@ import Payment from '../entity/payment.entity';
 
 export default class PaymentValidator implements ValidatorInterface<Payment> {
   validate(entity: Payment): ValidationError[] | void {
-    const productSchema = z.object({
+    const paymentSchema = z.object({
       _amount: z.number(),
       _userId: z.string(),
       _productId: z.array(z.string()),
     });
     try {
-      productSchema.parse(entity);
+      paymentSchema.parse(entity);
     } catch (error) {
       const zodError = error as z.ZodError;
       const errorMessages = zodError.errors.map((issue) => issue.message);
