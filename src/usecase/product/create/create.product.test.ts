9de1437ac7de970958ee099d0e68f87ef0ce7b36
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-01-22 11:14:19 -0300

    start create test in usecase

---
+++ src/usecase/product/create/create.product.test.ts
@@ -0,0 +1,86 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+import CreateProductUseCase from './create.product.usecase';
+import ProductRepositoryInterface from '../../../domain/product/repositories/product.repository.interface';
+import UploadImageInterface from '../../../infrastructure/product/uploadImage/product.uploadImage.interface';
+import { InputCreateProductDto } from './create.product.dto';
+
+const productRepositoryMock = {
+  create: vi.fn(),
+  find: vi.fn(),
+};
+
+const uploadImageMock = {
+  sendImage: vi.fn(),
+};
+
+vi.mock('../../../domain/product/factory/product.factory', async () => {
+	return {
+	  default: {
+		create: () => ({
+		  id: '123',
+		  name: "Produto Teste",
+		  gameItemName: "Item de Jogo",
+		  categoryId: "cat123",
+		  image: "imagem.jpg",
+		  price: 100,
+		  serverId: "server1",
+		}),
+	  },
+	};
+  });
+
+describe('CreateProductUseCase', () => {
+  let createProductUseCase: CreateProductUseCase;
+
+  beforeEach(() => {
+    createProductUseCase = new CreateProductUseCase(
+      productRepositoryMock as unknown as ProductRepositoryInterface,
+      uploadImageMock as unknown as UploadImageInterface
+    );
+  });
+
+  it('should create a product and return it`s data', async () => {
+	const input: InputCreateProductDto = {
+		name: "Produto Teste",
+		gameItemName: "Item de Jogo",
+		image: "imagem.jpg",
+		categoryId: "cat123",
+		price: 100,
+		serverId: "server1",
+	  };
+
+	productRepositoryMock.find.mockResolvedValueOnce({
+		id: '123',
+		name: input.name,
+		gameItemName: input.gameItemName,
+		categoryId: input.categoryId,
+		image: input.image,
+		price: input.price,
+		serverId: input.serverId,
+		category: {
+		  id: "cat123",
+		  name: "Categoria Teste",
+		  functionInGame: "Função no Jogo",
+		},
+	  });
+
+    const result = await createProductUseCase.execute(input);
+
+    expect(productRepositoryMock.create).toHaveBeenCalled();
+	expect(uploadImageMock.sendImage).toHaveBeenCalledWith(input.image);
+    expect(result).toEqual({
+		id: "123",
+		name: input.name,
+		gameItemName: input.gameItemName,
+		categoryId: input.categoryId,
+		image: input.image,
+		price: input.price,
+		serverId: input.serverId,
+		category: {
+		  id: "cat123",
+		  name: "Categoria Teste",
+		  functionInGame: "Função no Jogo",
+		},
+	  });
+  });
+});
---
+++ src/usecase/product/findAll/findAll.product.test.ts
@@ -0,0 +1,67 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+import FindAllProductUseCase from './findAll.product.usecase';
+import ProductRepositoryInterface from '../../../domain/product/repositories/product.repository.interface';
+import { InputFindAllProductDto } from './findAll.product.dto';
+
+const productRepositoryMock = {
+  findAll: vi.fn(),
+};
+
+describe('FindAllProductUseCase', () => {
+  let findAllProductUseCase: FindAllProductUseCase;
+
+  beforeEach(() => {
+    findAllProductUseCase = new FindAllProductUseCase(
+      productRepositoryMock as unknown as ProductRepositoryInterface
+    );
+  });
+
+  it('should retrieve all products and return them', async () => {
+
+    const input: InputFindAllProductDto = {
+      serverId: 'server1',
+      sort: 'asc',
+      categoryId: ['id1', 'id2'],
+    };
+
+    const mockProducts = [
+      {
+        id: '1',
+        name: 'Produto 1',
+        gameItemName: 'Item de Jogo 1',
+        categoryId: 'cat1',
+        image: 'imagem1.jpg',
+        price: 100,
+        serverId: 'serverId',
+        category: {
+          id: 'idCategory',
+          name: 'categoryid',
+          functionInGame: 'Função no Jogo 1',
+        },
+      },
+    ];
+
+    productRepositoryMock.findAll.mockResolvedValueOnce(mockProducts);
+
+    const result = await findAllProductUseCase.execute(input);
+
+    expect(productRepositoryMock.findAll).toHaveBeenCalledWith(
+      input.serverId, input.categoryId, input.sort
+    );
+
+    expect(result).toEqual(mockProducts.map(product => ({
+      id: product.id,
+      name: product.name,
+      gameItemName: product.gameItemName,
+      categoryId: product.categoryId,
+      image: product.image,
+      price: product.price,
+      serverId: product.serverId,
+      category: product.category ? {
+        id: product.category.id,
+        name: product.category.name,
+        functionInGame: product.category.functionInGame,
+      } : null,
+    })));
+  });
+});
