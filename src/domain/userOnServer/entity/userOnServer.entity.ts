1939f653bbf1182d978c7fb108e6f2d03dbd2b3f
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-08-11 20:01:04 -0300

    ♻️ change userOnServer entity

--- src/domain/userOnServer/entity/userOnServer.entity.ts
+++ src/domain/userOnServer/entity/userOnServer.entity.ts
@@ -1,35 +1,37 @@
+import type Server from "../../server/entity/server.entity";
+
 export default class UserOnServer {
-  private _id: string;
-  private _user: string;
-  private _server: string;
-  private _gameUserId: string;
-  private _nickname: string;
+	private _id: string;
+	private _user: string;
+	private _server: Server;
+	private _gameUserId: string;
+	private _nickname: string;
 
-  constructor(id: string, user: string, server: string, gameUserId: string, nickname: string) {
-    this._id = id;
-    this._user = user;
-    this._server = server;
-    this._gameUserId = gameUserId;
-    this._nickname = nickname;
-  }
+	constructor(id: string, user: string, server: Server, gameUserId: string, nickname: string) {
+		this._id = id;
+		this._user = user;
+		this._server = server;
+		this._gameUserId = gameUserId;
+		this._nickname = nickname;
+	}
 
-  get id(): string {
-    return this._id;
-  }
+	get id(): string {
+		return this._id;
+	}
 
-  get user(): string {
-    return this._user;
-  }
+	get user(): string {
+		return this._user;
+	}
 
-  get server(): string {
-    return this._server;
-  }
+	get server(): Server {
+		return this._server;
+	}
 
-  get gameUserId(): string {
-    return this._gameUserId;
-  }
+	get gameUserId(): string {
+		return this._gameUserId;
+	}
 
-  get nickname(): string {
-    return this._nickname;
-  }
+	get nickname(): string {
+		return this._nickname;
+	}
 }
--- src/domain/userOnServer/factory/userOnServe.factory.ts
+++ src/domain/userOnServer/factory/userOnServe.factory.ts
@@ -1,14 +1,9 @@
-import userOnServer from '../entity/userOnServer.entity';
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
+import type Server from "../../server/entity/server.entity";
+import userOnServer from "../entity/userOnServer.entity";
 
 export default class UserOnServerFactory {
-  static create(
-    user: string,
-    server: string,
-    gameUserId: string,
-    nickname: string,
-    id?: string
-  ): userOnServer {
-    return new userOnServer(id || uuid(), user, server, gameUserId, nickname);
-  }
+	static create(user: string, server: Server, gameUserId: string, nickname: string, id?: string): userOnServer {
+		return new userOnServer(id || uuid(), user, server, gameUserId, nickname);
+	}
 }
