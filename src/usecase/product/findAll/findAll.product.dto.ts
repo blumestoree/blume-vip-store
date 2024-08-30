84de94fc4f0996b995d05d7215877be29b9962c1
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-08-29 21:44:20 -0300

    ♻️ update usecase findall product

--- src/usecase/product/findAll/findAll.product.dto.ts
+++ src/usecase/product/findAll/findAll.product.dto.ts
@@ -1,18 +1,14 @@
 export interface InputFindAllProductDto {
-  serverId: string;
-  sort: 'desc' | 'asc' | undefined;
-  categoryId: string[] | undefined;
+	serverId: string;
+	sort: "desc" | "asc" | undefined;
+	categoryId: string[] | undefined;
 }
 export interface OutputFindAllProductDto {
-  id: string;
-  name: string;
-  gameItemName: string;
-  image: string;
-  price: number;
-  serverId: string;
-  category: {
-    functionInGame: string;
-    id: string;
-    name: string;
-  };
+	id: string;
+	name: string;
+	gameItemName: string;
+	image: string;
+	price: number;
+	serverId: string;
+	categoryId: string;
 }
--- src/usecase/product/findAll/findAll.product.usecase.ts
+++ src/usecase/product/findAll/findAll.product.usecase.ts
@@ -1,42 +1,27 @@
-import ProductRepositoryInterface from '../../../domain/product/repositories/product.repository.interface';
-import UseCaseInterface from '../../../shared/usecase.interface';
-import { InputFindAllProductDto, OutputFindAllProductDto } from './findAll.product.dto';
+import type ProductRepositoryInterface from "../../../domain/product/repositories/product.repository.interface";
+import type UseCaseInterface from "../../../shared/usecase.interface";
+import type { InputFindAllProductDto, OutputFindAllProductDto } from "./findAll.product.dto";
 
-export default class FindAllProductUseCase
-  implements UseCaseInterface<InputFindAllProductDto, OutputFindAllProductDto[]>
-{
-  constructor(private productRepository: ProductRepositoryInterface) {}
+export default class FindAllProductUseCase implements UseCaseInterface<InputFindAllProductDto, OutputFindAllProductDto[]> {
+	constructor(private productRepository: ProductRepositoryInterface) {}
 
-  async execute(input: InputFindAllProductDto): Promise<OutputFindAllProductDto[]> {
-    const allProducts = await this.productRepository.findAll(
-      input.serverId,
-      input.categoryId,
-      input.sort,
-    );
+	async execute(input: InputFindAllProductDto): Promise<OutputFindAllProductDto[]> {
+		const allProducts = await this.productRepository.findAll(input.serverId, input.categoryId, input.sort);
 
-    if (!allProducts) {
-      throw new Error('Product not found');
-    }
+		if (!allProducts) {
+			throw new Error("Product not found");
+		}
 
-    return allProducts.map((product) => {
-      
-      if (!product.category) {
-        throw new Error(`Category not found for product with id ${product.id}`);
-      }
-
-      return {
-        id: product.id,
-        name: product.name,
-        gameItemName: product.gameItemName,
-        image: product.image,
-        price: product.price,
-        serverId: product.serverId,
-        category: {
-          id: product.category.id,
-          name: product.category.name,
-          functionInGame: product.category.functionInGame,
-        }
-      };
-    });
-  }
+		return allProducts.map((product) => {
+			return {
+				id: product.id,
+				name: product.name,
+				gameItemName: product.gameItemName,
+				image: product.image,
+				price: product.price,
+				serverId: product.serverId,
+				categoryId: product.categoryId,
+			};
+		});
+	}
 }
