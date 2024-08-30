9721356d7862988d5f16f9f400f397ed012b7952
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-08-29 21:44:34 -0300

    ♻️ update usecase find product

--- src/usecase/product/find/find.product.dto.ts
+++ src/usecase/product/find/find.product.dto.ts
@@ -1,16 +1,12 @@
 export interface InputFindProductDto {
-  id: string;
+	id: string;
 }
 export interface OutputFindProductDto {
-  id: string;
-  name: string;
-  gameItemName: string;
-  image: string;
-  price: number;
-  serverId: string;
-  category: {
-    id: string;
-    name: string;
-    functionInGame: string;
-  };
+	id: string;
+	name: string;
+	gameItemName: string;
+	image: string;
+	price: number;
+	serverId: string;
+	categoryId: string;
 }
--- src/usecase/product/find/find.product.usecase.ts
+++ src/usecase/product/find/find.product.usecase.ts
@@ -1,35 +1,25 @@
-import ProductRepositoryInterface from '../../../domain/product/repositories/product.repository.interface';
-import UseCaseInterface from '../../../shared/usecase.interface';
-import { InputFindProductDto, OutputFindProductDto } from './find.product.dto';
+import type ProductRepositoryInterface from "../../../domain/product/repositories/product.repository.interface";
+import type UseCaseInterface from "../../../shared/usecase.interface";
+import type { InputFindProductDto, OutputFindProductDto } from "./find.product.dto";
 
-export default class FindProductUseCase
-  implements UseCaseInterface<InputFindProductDto, OutputFindProductDto>
-{
-  constructor(private productRepository: ProductRepositoryInterface) {}
+export default class FindProductUseCase implements UseCaseInterface<InputFindProductDto, OutputFindProductDto> {
+	constructor(private productRepository: ProductRepositoryInterface) {}
 
-  async execute(input: InputFindProductDto): Promise<OutputFindProductDto> {
-    const product = await this.productRepository.find(input.id);
+	async execute(input: InputFindProductDto): Promise<OutputFindProductDto> {
+		const product = await this.productRepository.find(input.id);
 
-    if (!product) {
-      throw new Error('Product not found');
-    }
+		if (!product) {
+			throw new Error("Product not found");
+		}
 
-    if (!product.category) {
-      throw new Error('Category not found for the product');
-    }
-
-    return {
-      id: product.id,
-      name: product.name,
-      gameItemName: product.gameItemName,
-      image: product.image,
-      price: product.price,
-      serverId: product.serverId,
-      category:  {
-            id: product.category.id,
-            name: product.category.name,
-            functionInGame: product.category.functionInGame,
-          },
-    };
-  }
+		return {
+			id: product.id,
+			name: product.name,
+			gameItemName: product.gameItemName,
+			image: product.image,
+			price: product.price,
+			serverId: product.serverId,
+			categoryId: product.categoryId,
+		};
+	}
 }
